27c45cf10872eb5be656bbbd2f4b8dc0cc3c7a8f
Austin Wickenberg <austinw6@109d25pf0fff8j7.local>
2019-02-04 12:51:38 -0700

    Initial Commit

---
+++ Github Example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Github Example
+//
+//  Created by Austin Wickenberg on 2019-02-01.
+//  Copyright © 2019 Austin Wickenberg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
